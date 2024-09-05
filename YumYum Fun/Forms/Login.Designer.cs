db43d1e1adae084beb89dc4ac9e82330e56da94c
Nukb <khalid159196@yahoo.com>
2024-09-05 16:07:21 +0200

    Add project files.

---
+++ YumYum Fun/Forms/Login.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace YumYum_Fun.Forms
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.guna2GradientPanel1 = new Guna.UI2.WinForms.Guna2GradientPanel();
+            this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.username_TextBox = new Guna.UI2.WinForms.Guna2TextBox();
+            this.password_TextBox = new Guna.UI2.WinForms.Guna2TextBox();
+            this.login_button = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2GradientPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2ControlBox1
+            // 
+            this.guna2ControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox1.FillColor = System.Drawing.Color.Coral;
+            this.guna2ControlBox1.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox1.Location = new System.Drawing.Point(377, 3);
+            this.guna2ControlBox1.Name = "guna2ControlBox1";
+            this.guna2ControlBox1.Size = new System.Drawing.Size(45, 29);
+            this.guna2ControlBox1.TabIndex = 0;
+            // 
+            // guna2GradientPanel1
+            // 
+            this.guna2GradientPanel1.BorderRadius = 80;
+            this.guna2GradientPanel1.Controls.Add(this.guna2PictureBox1);
+            this.guna2GradientPanel1.CustomizableEdges.TopLeft = false;
+            this.guna2GradientPanel1.CustomizableEdges.TopRight = false;
+            this.guna2GradientPanel1.FillColor = System.Drawing.Color.DodgerBlue;
+            this.guna2GradientPanel1.FillColor2 = System.Drawing.Color.DodgerBlue;
+            this.guna2GradientPanel1.Location = new System.Drawing.Point(-3, -1);
+            this.guna2GradientPanel1.Name = "guna2GradientPanel1";
+            this.guna2GradientPanel1.Size = new System.Drawing.Size(432, 256);
+            this.guna2GradientPanel1.TabIndex = 1;
+            // 
+            // guna2PictureBox1
+            // 
+            this.guna2PictureBox1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.guna2PictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("guna2PictureBox1.Image")));
+            this.guna2PictureBox1.ImageRotate = 0F;
+            this.guna2PictureBox1.Location = new System.Drawing.Point(127, 47);
+            this.guna2PictureBox1.Name = "guna2PictureBox1";
+            this.guna2PictureBox1.Size = new System.Drawing.Size(170, 170);
+            this.guna2PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.guna2PictureBox1.TabIndex = 0;
+            this.guna2PictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Zain", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.World, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(148, 293);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 28);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "اسم المستخدم\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Zain", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.World, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(160, 412);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 28);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "كلمة المرور";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // username_TextBox
+            // 
+            this.username_TextBox.BorderColor = System.Drawing.Color.Black;
+            this.username_TextBox.BorderRadius = 5;
+            this.username_TextBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.username_TextBox.DefaultText = "";
+            this.username_TextBox.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.username_TextBox.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.username_TextBox.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.username_TextBox.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.username_TextBox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.username_TextBox.Font = new System.Drawing.Font("Zain", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.World, ((byte)(0)));
+            this.username_TextBox.ForeColor = System.Drawing.Color.Black;
+            this.username_TextBox.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.username_TextBox.Location = new System.Drawing.Point(87, 335);
+            this.username_TextBox.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.username_TextBox.Name = "username_TextBox";
+            this.username_TextBox.PasswordChar = '\0';
+            this.username_TextBox.PlaceholderText = "";
+            this.username_TextBox.SelectedText = "";
+            this.username_TextBox.Size = new System.Drawing.Size(256, 38);
+            this.username_TextBox.TabIndex = 5;
+            this.username_TextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // password_TextBox
+            // 
+            this.password_TextBox.BorderColor = System.Drawing.Color.Black;
+            this.password_TextBox.BorderRadius = 5;
+            this.password_TextBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.password_TextBox.DefaultText = "";
+            this.password_TextBox.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.password_TextBox.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.password_TextBox.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.password_TextBox.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.password_TextBox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.password_TextBox.Font = new System.Drawing.Font("Zain", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.World, ((byte)(0)));
+            this.password_TextBox.ForeColor = System.Drawing.Color.Black;
+            this.password_TextBox.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.password_TextBox.Location = new System.Drawing.Point(87, 452);
+            this.password_TextBox.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.password_TextBox.Name = "password_TextBox";
+            this.password_TextBox.PasswordChar = '*';
+            this.password_TextBox.PlaceholderText = "";
+            this.password_TextBox.SelectedText = "";
+            this.password_TextBox.Size = new System.Drawing.Size(256, 38);
+            this.password_TextBox.TabIndex = 6;
+            this.password_TextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // login_button
+            // 
+            this.login_button.BorderRadius = 20;
+            this.login_button.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.login_button.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.login_button.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.login_button.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.login_button.FillColor = System.Drawing.Color.Coral;
+            this.login_button.Font = new System.Drawing.Font("Zain", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.World, ((byte)(0)));
+            this.login_button.ForeColor = System.Drawing.Color.White;
+            this.login_button.Location = new System.Drawing.Point(122, 544);
+            this.login_button.Name = "login_button";
+            this.login_button.Size = new System.Drawing.Size(180, 45);
+            this.login_button.TabIndex = 7;
+            this.login_button.Text = "موافق";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(428, 630);
+            this.Controls.Add(this.login_button);
+            this.Controls.Add(this.password_TextBox);
+            this.Controls.Add(this.username_TextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.guna2ControlBox1);
+            this.Controls.Add(this.guna2GradientPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Login";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.guna2GradientPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
+        private Guna.UI2.WinForms.Guna2GradientPanel guna2GradientPanel1;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private Guna.UI2.WinForms.Guna2TextBox username_TextBox;
+        private Guna.UI2.WinForms.Guna2TextBox password_TextBox;
+        private Guna.UI2.WinForms.Guna2Button login_button;
+    }
+}
---
+++ YumYum Fun/Forms/Main.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace YumYum_Fun
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.general_Panel = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.minimize_box = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.close_box = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.guna2GradientPanel1 = new Guna.UI2.WinForms.Guna2GradientPanel();
+            this.general_Panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
+            this.guna2GradientPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // general_Panel
+            // 
+            this.general_Panel.Controls.Add(this.guna2GradientPanel1);
+            this.general_Panel.Controls.Add(this.minimize_box);
+            this.general_Panel.Controls.Add(this.close_box);
+            this.general_Panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.general_Panel.Location = new System.Drawing.Point(0, 0);
+            this.general_Panel.Name = "general_Panel";
+            this.general_Panel.Size = new System.Drawing.Size(1600, 860);
+            this.general_Panel.TabIndex = 0;
+            // 
+            // guna2PictureBox1
+            // 
+            this.guna2PictureBox1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.guna2PictureBox1.FillColor = System.Drawing.Color.DodgerBlue;
+            this.guna2PictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("guna2PictureBox1.Image")));
+            this.guna2PictureBox1.ImageRotate = 0F;
+            this.guna2PictureBox1.Location = new System.Drawing.Point(13, 22);
+            this.guna2PictureBox1.Name = "guna2PictureBox1";
+            this.guna2PictureBox1.Size = new System.Drawing.Size(120, 120);
+            this.guna2PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.guna2PictureBox1.TabIndex = 0;
+            this.guna2PictureBox1.TabStop = false;
+            // 
+            // minimize_box
+            // 
+            this.minimize_box.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.minimize_box.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MinimizeBox;
+            this.minimize_box.FillColor = System.Drawing.Color.Coral;
+            this.minimize_box.IconColor = System.Drawing.Color.White;
+            this.minimize_box.Location = new System.Drawing.Point(54, 3);
+            this.minimize_box.Name = "minimize_box";
+            this.minimize_box.Size = new System.Drawing.Size(45, 29);
+            this.minimize_box.TabIndex = 1;
+            // 
+            // close_box
+            // 
+            this.close_box.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.close_box.FillColor = System.Drawing.Color.Coral;
+            this.close_box.IconColor = System.Drawing.Color.White;
+            this.close_box.Location = new System.Drawing.Point(3, 3);
+            this.close_box.Name = "close_box";
+            this.close_box.Size = new System.Drawing.Size(45, 29);
+            this.close_box.TabIndex = 0;
+            // 
+            // guna2GradientPanel1
+            // 
+            this.guna2GradientPanel1.BorderRadius = 40;
+            this.guna2GradientPanel1.Controls.Add(this.guna2PictureBox1);
+            this.guna2GradientPanel1.FillColor = System.Drawing.Color.DodgerBlue;
+            this.guna2GradientPanel1.FillColor2 = System.Drawing.Color.DodgerBlue;
+            this.guna2GradientPanel1.Location = new System.Drawing.Point(1443, 12);
+            this.guna2GradientPanel1.Name = "guna2GradientPanel1";
+            this.guna2GradientPanel1.Size = new System.Drawing.Size(145, 836);
+            this.guna2GradientPanel1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1600, 860);
+            this.Controls.Add(this.general_Panel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.general_Panel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
+            this.guna2GradientPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel general_Panel;
+        private Guna.UI2.WinForms.Guna2ControlBox minimize_box;
+        private Guna.UI2.WinForms.Guna2ControlBox close_box;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private Guna.UI2.WinForms.Guna2GradientPanel guna2GradientPanel1;
+    }
+}
+
